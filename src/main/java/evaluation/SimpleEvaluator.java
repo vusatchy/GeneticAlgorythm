f803a48f33eb43e70da094af63dd029fcebbc8cf
vusatchy <vusatch.y@gmail.com>
2019-02-18 21:19:08 +0200

    simple evaluator simplified

--- src/main/java/evaluation/SimpleEvaluator.java
+++ src/main/java/evaluation/SimpleEvaluator.java
@@ -14,8 +14,6 @@ public class SimpleEvaluator implements Evaluator {
 
     @Override
     public double evaluate(BitEntity bitEntity, double k, double b) {
-        double x = bitEntity.getNumericValue();
-        x = k * x + b;
-        return function.apply(x);
+        return function.apply(k * bitEntity.getNumericValue() + b);
     }
 }
